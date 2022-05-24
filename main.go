ac4234aecfceccde1498eafdb0c9ed7d072a2478
SKKUGoon <baesangil0906@gmail.com>
2022-05-24 17:43:15 +0900

    Add report parser => to get raw report from the iframe html

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"goKorMktEvent/watch/dart"
+	"goKorMktEvent/watch/parsing"
 	"log"
 	"time"
 )
 
 func main() {
 	for {
-		p, _ := dart.Rss()
+		reports, _ := dart.Rss()
+		if len(reports) == 0 {
+			//time.Sleep(10 * time.Second)
+			log.Println("no info")
+			break
+		} else {
+			log.Println(reports)
+		}
 		time.Sleep(10 * time.Second)
-
-		log.Println(p)
 	}
+	parsing.RawReportText("http://dart.fss.or.kr/dsaf001/main.do?rcpNo=20220523000062")
 }
--- watch/parsing/reportParse.go
+++ watch/parsing/reportParse.go
@@ -1,1 +1,136 @@
 package parsing
+
+import (
+	"errors"
+	"fmt"
+	"goKorMktEvent/watch"
+	"golang.org/x/net/html"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+const (
+	// dcmNo parsing info
+	dcmId     = "'dcmNo'"
+	dcmLength = 7
+
+	// rcpNo parsing info
+	rcpId     = "'rcpNo'"
+	rcpLength = 14
+
+	dcmParseIndSrt = len(dcmId) + len("] = '")
+	rcpParseIndSrt = len(rcpId) + len("] = '")
+)
+
+const (
+	bonusIssueInfo = "1주당 신주배정 주식수"
+)
+
+func GetReportText(u string) (string, error) {
+	resp, err := http.Get(u)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return "", errors.New(resp.Status)
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	} else {
+		return string(data), nil
+	}
+}
+
+func getDcmNo(ctx string) (string, string) {
+	i := strings.Index(ctx, dcmId)
+	dcmNo := ctx[i+dcmParseIndSrt : i+dcmParseIndSrt+dcmLength]
+	return "dcmNo", dcmNo
+}
+
+func getRcpNo(ctx string) (string, string) {
+	i := strings.Index(ctx, rcpId)
+	rcpNo := ctx[i+rcpParseIndSrt : i+rcpParseIndSrt+rcpLength]
+	return "rcpNo", rcpNo
+}
+
+func RawReportURL(ctx string) (string, error) {
+	// Create Parameter HashMap
+	dcmKey, dcmVal := getDcmNo(ctx)
+	rcpKey, rcpVal := getRcpNo(ctx)
+	var rawReportURL = "http://dart.fss.or.kr/report/viewer.do?"
+	var rawReportParam = watch.ReportMap{
+		dcmKey:   dcmVal,
+		rcpKey:   rcpVal,
+		"eleId":  "0",
+		"offset": "0",
+		"length": "0",
+		"dtd":    "dart3.xsd",
+	}
+
+	// Create Request
+	resp, err := http.NewRequest("GET", rawReportURL, nil)
+	if err != nil {
+		log.Println(err)
+		return "", err
+	}
+
+	// Add Parameter
+	qry := resp.URL.Query()
+	for key, element := range rawReportParam {
+		qry.Add(key, element)
+	}
+
+	resp.URL.RawQuery = qry.Encode()
+
+	return resp.URL.String(), nil
+}
+
+func RawReportText(source string) {
+	c, _ := GetReportText(source)
+
+	u, _ := RawReportURL(c)
+	fmt.Println("url", u)
+
+	resp, _ := http.Get(u)
+	defer resp.Body.Close()
+	data, _ := ioutil.ReadAll(resp.Body)
+	_ = ReportContent(string(data))
+}
+
+func ReportContent(text string) watch.BonusIssue {
+	var container = watch.BonusIssue{}
+
+	tkn := html.NewTokenizer(strings.NewReader(text))
+	for {
+		tt := tkn.Next()
+
+		switch {
+		// End of parsing
+		case tt == html.ErrorToken:
+			return container
+
+		// ex) <a> is a StartTagToken
+		case tt == html.StartTagToken:
+			tn := tkn.Token()
+			switch {
+			case tn.Data == "td":
+				tt, tn = tkn.Next(), tkn.Token()
+				_ = bonusIssueFill(tn)
+			default:
+				continue
+			}
+		}
+	}
+}
+
+func bonusIssueFill(t html.Token) string {
+	data := strings.ReplaceAll(t.Data, "&nbsp", "")
+	fmt.Println(data)
+	return ""
+}
